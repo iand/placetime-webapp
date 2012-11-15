c8967f52ab9ed61474b3661b23b7040888e50b07
Ian Davis <me@iandavis.com>
2012-11-15 08:49:32 +0000

    Hooked up ui buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,11 @@ $(function(){
         template: _.template($("#item-template").html()),
         templatefeature: _.template($("#itemfeature-template").html()),
 
+        events:{
+          'click .addbtn'        :  'promote',
+          'click .followbtn'        :  'follow'
+        },
+
 
         render: function(eventName) {
 
@@ -27,28 +32,71 @@ $(function(){
             $(this.el).append(lTemplate);
           }, this);
           return this;
+        },
+
+
+        promote: function(eventName) {
+          alert("Promote item to maybe list");
+        },
+
+        follow: function(eventName) {
+          alert("Follow this user");
         }
 
     });
+
+
 
 
     items = new ItemList();
     
 
     var AppView = Backbone.View.extend({
-      el: "body",
+      el: "body"
 
-      render: function(){
+
+      ,events:{
+           'click #possiblebtn'        :  'possible'
+          ,'click #maybebtn'           :  'maybe'
+          ,'click #newbtn'             :  'newitem'
+          ,'click #addedbtn'           :  'added'
+          ,'click #calendarbtn'        :  'calendar'
+      }
+
+      ,render: function(){
         var lItemsView = new ItemsView({model:items});
         var lHtml = lItemsView.render().el;
         $('#items').html(lHtml);
-      },
+      }
 
-      initialize: function(){
+      ,initialize: function(){
         var lOptions = {};
         lOptions.success = this.render;
         items.fetch(lOptions);
       }
+
+
+      ,possible: function(eventName) {
+        alert("View possible items");
+      }
+
+
+      ,maybe: function(eventName) {
+        alert("View maybe items");
+      }
+
+      ,newitem: function(eventName) {
+        alert("Add a new item");
+      }
+
+      ,added: function(eventName) {
+        alert("Sort by date added");
+      }
+
+      ,calendar: function(eventName) {
+        alert("Sort by event date");
+      }      
+
     });
 
     var App = new AppView;
