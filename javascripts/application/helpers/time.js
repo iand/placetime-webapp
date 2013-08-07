ef579d173f370ad61cba09d5fc327611fc1c8e50
Ian Davis <nospam@iandavis.com>
2013-08-07 13:40:19 +0100

    Fixed moment typo

--- javascripts/application/helpers/time.js
+++ javascripts/application/helpers/time.js
@@ -6,7 +6,7 @@ Handlebars.registerHelper('fromNow', function(value) {
 
 Handlebars.registerHelper('fromNowAdded', function(value) {
  
-    if (moment.diff(moment(), moment.unix(value.toString().substr(0, 10)) ) < 3000) {
+    if (moment().diff(moment(), moment.unix(value.toString().substr(0, 10)) ) < 3000) {
         return "just now";
     } else {
         return moment.unix(
