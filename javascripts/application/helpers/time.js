25e1e849bab2f17920e30ece8a96c1485e980fb6
Ian Davis <nospam@iandavis.com>
2013-08-07 13:34:16 +0100

    Added special display for added times in near future due to clock skew

--- javascripts/application/helpers/time.js
+++ javascripts/application/helpers/time.js
@@ -3,3 +3,14 @@ Handlebars.registerHelper('fromNow', function(value) {
         value.toString().substr(0, 10)
     ).fromNow();
 });
+
+Handlebars.registerHelper('fromNowAdded', function(value) {
+ 
+    if (moment.diff(moment(), moment.unix(value.toString().substr(0, 10)) ) < 3000) {
+        return "just now";
+    } else {
+        return moment.unix(
+            value.toString().substr(0, 10)
+        ).fromNow();
+    }
+});
